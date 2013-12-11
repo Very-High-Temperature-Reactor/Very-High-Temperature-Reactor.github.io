f038af36a44e1544f9bdf2c5904a5d3df933b0f6
Mateusz Galazyn <Mateusz.Galazyn@gmail.com>
2013-12-11 14:07:03 +0100

    fuel

--- contents/fuel.js
+++ contents/fuel.js
@@ -1,7 +1,35 @@
 contents.fuel = {
   index: 2,
-  caption: "Paliwo dla reaktorów VHTR",
-  text: '<p id="one">' +
-'' +
-'</p>'
+  caption: "Paliwo dla reaktorów wysokotemperaturowych",
+  text: '<p id="fuel">Podstawowym elementem paliwowym dla VHTR są kapsułki z tlenkiem lub węglikiem uranu pokryte czterema wartstwami warstwami ochronnymi. W zależności od typu rdzenia reaktora, granulki ułożone są w sześciokątnych blokach z grafitu, albo grafitowych kulach.</p>'+
+  '<h1 id="triso">Kapsułki TRISO</h1>'+
+  '<p>Na rys. 1 została przedstawiona budowa kapsułki TRISO (<i>ang.</i>TRIple ISOtropic</i>). W jej wnętrzu znajduje się niskowzbogacony (do 15%) uran. Niekiedy dodawany jest też paliworodny tor.' +
+  '<div class="thumbnail">'+
+  '<img src="http://nowa.ncbj.edu.pl/sites/default/files/styles/large/public/field/image/paliwo1_1800.jpg" alt="Schemat budowy TRISO" class="img-thumbnail" width="90%"/>'+
+  '<div class="caption">Rys. 1. Budowa kapsułki TRISO <a name="b_ncbj_triso"></a>.</div>'+
+  '</div>'+
+  'Jądro granulki jest opakowane w cztery warstwy: buforową węgla, pirowęgla, SiC lub ZrC i znów pirowęgla. Dwie wewnętrzne warstwy węglowe zapobiegają kontaktowi warstwy ceramicznej z gazami będącymi produktami rozpadu. Pełnią też rolę ochronną przed nackiskiem z powodu puchnącego jądra kapsułki. Ceramiczna warstwa SiC lub ZrC zachowuje się jak miniaturowy pojemnik ciśnieniowy utrzymujący wszystkie produkty rozpadu w kapsułce. Jest ona w stanie wytrzymać temperatury poniżej 1600&deg;C, co wyznacza temperaturę graniczną paliwa w przypadku awarii. Zewnętrzna warstwa węglowa chroni przed uszkodzeniami mechanicznymi paliwa w trakcie pracy.</p>'+
+  '</p>'+
+  '<h1 id="fuel_elements">Elementy paliwowe</h1>'+
+  'Kapsułki TRISO są łączone z pyłem grafitowym, oraz spajaczami. Z tak powstałej mieszaniny formowane są elementy paliwowe odpowiednie dla danego typu rdzenia.'+
+  '<p><h2>Sześciokątne bloki</h2>'+
+  'W stosunku do kul grafitowych, pryzmatyczny rdzeń zapewnia lepsze chłodzenie, przez co reaktor jest w stanie osiągnąć większą gęstość mocy, zatem i większą wynikową moc. '+
+  '<div class="thumbnail">'+
+  '<img src="http://nowa.ncbj.edu.pl/sites/default/files/styles/large/public/field/image/paliwo5_1800.jpg" alt="Budowa bloku pryzmatycznego" class="img-thumbnail" width="90%"/>'+
+  '<div class="caption">Rys. 3. Budowa rdzenia pryzmatycznego oraz elementu paliwowego. Z kapsułek TRISO uformowane są pręty z przewodami chłodzącymi, które są następnie wstawione w grafitowy blok. Wysokość takiej konstrukcji wynosi ok. 80 cm. <a name="b_ncbj_triso"></a>.</div>'+
+  '</div>'+
+  '<h2>Kule grafitowe</h2>'+
+  'Zaletą tego rozwiązania jest łatwiejsza wymiana paliwa niewymagająca wyłączania reaktora. Niestety to rozwiązanie jest droższe w stosunku do sześciokątnych bloków paliwowych.'+
+  '<div class="thumbnail">'+
+  '<img src="http://nowa.ncbj.edu.pl/sites/default/files/styles/large/public/field/image/paliwo2_1800.jpg" alt="Budowa kuli grafitowej" class="img-thumbnail" width="80%"/>'+
+  '<div class="caption">Rys. 4. Budowa kul paliwowych, dla reaktora z rdzeniem usypanym. <a name="b_ncbj_triso"></a>.</div>'+
+  '</div>'+
+  '</p>',
+  bibliography: {
+    celin: 'Z. Celiński: <i>Energetyka Jądrowa</i> ',
+    ncbj_triso: '<a href="http://ncbj.edu.pl/typy-reaktorow/triso-czastki-paliwa-dla-reaktorow-htgr">http://ncbj.edu.pl/typy-reaktorow/triso-czastki-paliwa-dla-reaktorow-htgr</a>',
+    wiki_triso: '<a href="http://en.wikipedia.org/wiki/Nuclear_fuel#TRISO_fuel">http://en.wikipedia.org/wiki/Nuclear_fuel#TRISO_fuel</a>',
+    vhtr_rep: '<a href="http://large.stanford.edu/courses/2013/ph241/kallman1/docs/chapin.pdf"><i>A Very High Temperature Reactor: A Technical Summary</i></a>',
+
+  }
 };

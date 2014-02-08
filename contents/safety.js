9fa53eee9d704e559c434d5446c568d044172039
Mateusz Galazyn <Mateusz.Galazyn@gmail.com>
2014-02-08 16:22:00 +0100

    synthtetic fuel

---
+++ contents/safety.js
@@ -0,0 +1,8 @@
+contents.safety = {
+  index: 8,
+  caption: "Bezpieczeństwo",
+  text: '',
+  bibliography: {
+    dieselnet: '<a href="https://www.dieselnet.com/tech/fuel_syn.php#intro">https://www.dieselnet.com/tech/fuel_syn.php#intro</a>'
+  }
+};
--- contents/synthetic_fuel.js
+++ contents/synthetic_fuel.js
@@ -1,7 +1,7 @@
 contents.synthetic_fuel = {
   index: 7,
   caption: "Produkcja paliwa syntetycznego",
-  text: '<p id="syntetic_fuel">Paliwa syntetyczne jest konkurentem dla paliwa naturalnego z powodu stale rosnących cen ropy naftowej. W poniższej tabeli zostało przedstawione oszacowanie kosztów produkcji baryłki danego rodzaju paliwa. Ceny z roku 2002 <a name="b_dieselnet"></a>. </p>'+
+  text: '<p id="synthetic_fuel">Dużą zaletą paliwa syntetycznego w stosunku do naturalnego jest brak zawartości siarki i azotu, które mają ujemny wpływ na środowisko. Dodatkowo, jego konkurencyjność zwiększają stale rosnące ceny ropy naftowej. W poniższej tabeli zostało przedstawione oszacowanie kosztów produkcji baryłki danego rodzaju paliwa. Ceny z roku 2002 <a name="b_dieselnet"></a>. </p>'+
   '<table class="table table-bordered">'+
   '<tr class="gray">'+
     '<th>Komponent ceny</th>'+
@@ -42,23 +42,38 @@ contents.synthetic_fuel = {
   '<small>1 MMBtu = 10<sup>6</sup> BTU = 1055 MJ</small><br/><br/>'+
   '<p>Zgodnie z nowymi oszacowaniami z 2008 r. paliwa syntetyczne będą ekonomiczne przy cenie gazu ziemnego $15/MCF (1 MCF = 1000 ft<sup>3</sup>) i ropy $120/Bbl. Zauważyć można, że w przpadku paliw naturalnych, cena w dużej mierze zależy od ceny ropy naftowej. Jeżeli chodzi o paliwa syntetyczne - cena surowców wynosi mniej niż 1/4 ceny, co wpływa na stabilność ceny produktu.</p>'+
   '<p>Metody produkcji paliwa syntetycznego można podzielić na dwie grupy: konwersję pośrednią i bezpośrednią. Konwersja pośrednia opiera się na pierwotnej konwersji substratów do syngazu (mieszaniny H<sub>2</sub> i CO), a następnie formowanie z niego węglowodorów. Wiodące technologie w konwersji poprzez syngaz to synteza Fischera-Tropscha i metoda MTG (<i>ang.</i> methanol to gasoline). Metody bezpośrednie mogą być ogólnie podzielone na dwa typy: pirolizę i uwęglenie - proces Kerricka oraz uwodornienie - metoda Bergiusa.</p>'+
+  //---------------
   '<h1>Konwersja pośrednia</h1>'+
   '<p>Na początku biomasa, gaz ziemny, albo węgiel jest transformowany w syngaz. Następnie za pomocą jednej z dwóch rodzajów metod są wytwarzane syntetyczne paliwa.</p>'+
+  //---------------
   '<h2>Proces Fischera-Tropscha</h2>'+
   '<p>Nazwa tej metody pochodzi od nazwisk jej wynalazców: Franza Fischera i Hansa Tropscha. Metoda ta była używana w trakcie II wojny światowej do zaopatrywania armii niemieckiej w paliwo. Proces Fischera-Tropscha (F-T) jest katalityczną reakcją, w której tlenek węgla i wodór w syngazie są konwertowane do różnorodnych węglowodorów wg. poniższej reakcji:<br/>(2n+1) H<sub>2</sub> + n CO &rarr; C<sub>n</sub> H<sub>(2n+2)</sub> + n H<sub>2</sub>O.<br/>Inną reakcją która także zachodzi podczas tego procesu jest konwersja CO + H<sub>2</sub>O &harr; CO<sub>2</sub> + H<sub>2</sub>. W trakcie metody Fischera-Tropscha pożądane jest osiągnięcie jak najdłuższych łańcuchów węglowodorowych, które przedstawiają największą wartość. W związku z tym, niezbędny jest katalizator tani, faworyzujący węglowodory o długich łańcuchach. Powszechnie stosowanym materiałem do tego celu jest żelazo. Atrakcyjnym pierwiastkiem jest także kobalt, który ma większą aktywność w konwersji CO, co ma znaczenie przy przetwarzaniu syngazu ubogiego w wodór (pochodzącego np. z gazyfikacji węgla). W stosunku do żelaza jest on jednak 230 razy droższy i faworyzuje tworzenie metanu w wyższych temperaturach.</p>'+
   '<p>Synteza Fischera-Tropscha musi być przeprowadzana w temperaturze 150 - 300 &deg;C i jest reakcją egzotermiczną. Ważne jest odprowadzanie ciepła, ponieważ w wyższych temperaturach zwiększa się produkcja metanu. Zwiększenie ciśnienia w reaktorze powoduje wydłużenie łańcuchów węglowodorowych i zwiększenie efektywności reakcji.</p>'+
   '<p>Użycie ciepła z elektrownii jądrowej pozwala na zredukowanie zapotrzebowania na węgiel w tym procesie. Produkcja 25.000 baryłek paliwa syntetycznego z wykorzystaniem węgla jako substratu jak i paliwa do podgrzewania reakcji gazyfikacji i formacji za pomocą procesu Fischera-Tropscha wymaga zużycia 14.600 ton węgla. Dodatkowo jest emitowana duża ilosć CO<sub>2</sub> do atmosfery. Wykorzystanie ciepła reaktorowego do elektrolizy wody i procesu F-T pozwala wyprodukować tą samą ilość paliwa zużywając tylko 4.400 ton węgla i wydzielając stosunkowo niewielką ilość CO<sub>2</sub>.</p>'+
+  '<div class="thumbnail">'+
+    '<iframe width="640" height="360" src="http://www.youtube.com/embed/44OU4JxEK4k?rel=0" frameborder="0" allowfullscreen></iframe>'+
+    '<div class="caption">Film. 1. Film ilustrujący reakcję Fischera-Tropscha. Atomy węgla są szare, atomy tlenu czerwone, wodór biały, a ruten jest zielony.</div>'+
+  '</div>'+
+  //---------------
   '<h2>Metoda MTG</h2>'+
+  '<p>Metoda MTG (methanol to gasoline) opiera się na konwersji syngazu do metanolu, a następnie jego polimeryzacji z wykorzystaniem <a href="http://pl.wikipedia.org/wiki/Zeolit">zeolitu</a>.</p><p>Produkcja metanolu odbywa się z wykorzystaniem poniższych reakcji:<br/>2 H<sub>2</sub> + CO &rarr; CH<sub>3</sub>OH<br/> CO<sub>2</sub> + 3 H<sub>2</sub> &rarr; CH<sub>3</sub>OH + H<sub>2</sub>O<br/> CO + H<sub>2</sub>O &rarr; CO<sub>2</sub> + H<sub>2</sub><br/>Trzecia z wyżej wymienionych reakcji jest reakcją konwersji CO, która jest niezbędna zwiększenia stężenia wodoru w syngazie. Całkowita konwersja do metanolu zachodzi przy stosunku stężeń H<sub>2</sub>/CO wynoszącym ok. 2. Cały proces jest egzotermiczny i jest przeprowadzany w reaktorach z przytwierdzonym katalizatorem pod ciśnieniem 4 - 11 MPa i temperaturą z zakresu 200 - 300 &deg;C. Katalizatorami używanymi w syntezie metanolu są: tlenek cynku, miedź, tlenek glinu i tlenek magnezu. </p>'+
+  '<p>Metanol jest odwadniany do benzyny za pomocą procesu opracowanego przez firmę Mobil:<br/>2 CH<sub>3</sub>OH &harr; CH<sub>3</sub>OCH<sub>3</sub> + H<sub>2</sub>O<br/>Następnie jest on dalej odwadniany przy pomocy katalizatora z zeolitu, ZSM-5, aż do momentu otrzymania benzyny z zawartością długich węglowodorów (C<sub>4</sub>-C<sub>10</sub>) większą niż 80%. </p>'+
+  //---------------
   '<h1>Konwersja bezpośrednia</h1>'+
   '<p>W tych metodach omija się fazę tworzenia syngazu, produkując bezpośrednio paliwo z węgla lub biomasy.</p>'+
+  //---------------
   '<h2>Metoda Bergiusa</h2>'+
-  '<p></p>'+
+  '<p>Proces ten opiera się na uwodornieniu węgla w wysokiej temperaturze (400 - 500 &deg;C) i ciśnieniu (20 - 70 MPa).  W tej metodzie węgiel jest upłynniany poprzez mieszanie go z wodorem i podgrzewanie w obecności katalizatora. Katalizatorami używanymi w tej reakcji są siarczki molibdenu i wolframu oraz oleiniany cyny i niklu. Zachodzącą reakcję można podsumować:<br/>n C + (n+1) H<sub>2</sub> &rarr; C<sub>n</sub>H<sub>2n+2</sub><br/>Produkty reakcji muszą jeszcze zostać poddane stabilizacji za pomocą katalizatorów używanych w <a href="http://en.wikipedia.org/wiki/Hydrodesulfurization">odsiarczaniu</a>. Wśród produktów reakcji znajduje się duża zawartość związków aromatycznych oraz mała ilosć parafin i olein. Poszczególne frakcje można poddać dalszym procesom rafinacji, aby otrzymać paliwo syntetyczne o żądanej jakości. Ponad 97% węgla będącego substratem reakcji jest konwertowane w paliwo syntetyczne, pozostały węgiel użyty do produkcji wodoru będzie zamieniony w CO<sub>2</sub> redukując efektywność reakcji i zwiększając emisję CO<sub>2</sub>.</p>'+
+  //---------------  
   '<h2>Metoda Karricka</h2>'+
-  '<p></p>',
+  '<p>Istnieje wiele przykładów procesów <a href="http://pl.wikipedia.org/wiki/Uwęglenie">uwęglenia</a>. Uwęglenie następuje poprzez <a href="http://pl.wikipedia.org/wiki/Piroliza">pirolizę</a>, a jego produktami jest smoła drzewna, oleje, para wodna i gaz syntetyczny. Smoły i oleje się uwodornia, aby usunąć związki siarki i azotu, a następnie przetwarza w paliwa. Typowym przykładem procesu uwęglania jest metoda Karricka.</p>'+
+  '<p>Węgiel znajduje się w strumieniu przegranej pary. W miarę wzrostu temperatury wegla rozpoczyna się piroliza (w zakresie 450 - 700 &deg;C) bez dostępu powietrza. W tej temperaturze produkowana smoła jest bogatsza w lżejsze węglowodory niż zwykła smoła węglowa, zatem jest odpowiednim substratem do procesu konwersji do paliwa. Proces Karricka nie generuje CO<sub>2</sub> - zamist tego produktem w tym procesie jest CO.</p>',
   bibliography: {
-    dieselnet: '<a href="">https://www.dieselnet.com/tech/fuel_syn.php#intro</a>',
+    dieselnet: '<a href="https://www.dieselnet.com/tech/fuel_syn.php#intro">https://www.dieselnet.com/tech/fuel_syn.php#intro</a>',
     wiki_sf: '<a href="http://en.wikipedia.org/wiki/Synthetic_fuel">http://en.wikipedia.org/wiki/Synthetic_fuel</a>',
     gasifi_ft: '<a href="http://www.netl.doe.gov/research/coal/energy-systems/gasification/gasifipedia/ftsynthesis">http://www.netl.doe.gov/research/coal/energy-systems/gasification/gasifipedia/ftsynthesis</a>',
     wn_process_heat: '<a href="http://www.world-nuclear.org/info/Non-Power-Nuclear-Applications/Industry/Nuclear-Process-Heat-for-Industry/">http://www.world-nuclear.org/info/Non-Power-Nuclear-Applications/Industry/Nuclear-Process-Heat-for-Industry/</a>',
+    gasifi_stm: '<a href="http://www.netl.doe.gov/research/coal/energy-systems/gasification/gasifipedia/methanol">http://www.netl.doe.gov/research/coal/energy-systems/gasification/gasifipedia/methanol</a>',
+    nzic_nz: '<a href="http://nzic.org.nz/ChemProcesses/energy/7D.pdf"><i>The Production of Methanol and Gasoline</i> - New Zealand Institute of Chemistry</a>',
   }
 };

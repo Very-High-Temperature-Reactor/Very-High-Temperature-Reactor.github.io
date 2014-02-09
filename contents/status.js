ce1719f8ad3e4e91c690cc568f84faec80727b5f
Mateusz Galazyn <Mateusz.Galazyn@gmail.com>
2014-02-09 02:08:58 +0100

    synergy

---
+++ contents/status.js
@@ -0,0 +1,8 @@
+contents.status = {
+  index: 9,
+  caption: "Status technologii i wdrożeń VHTR",
+  text: '<p id="status"></p>',
+  bibliography: {
+    vhtr_rep: '<a href="http://large.stanford.edu/courses/2013/ph241/kallman1/docs/chapin.pdf"><i>A Very High Temperature Reactor: A Technical Summary</i></a>'
+  }
+};

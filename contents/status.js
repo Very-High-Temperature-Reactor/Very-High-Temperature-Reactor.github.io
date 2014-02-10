e13378a397e0c1dfd380c5db746e8c0706a7a186
Mateusz Galazyn <Mateusz.Galazyn@gmail.com>
2014-02-10 03:06:46 +0100

    complete work

--- contents/status.js
+++ contents/status.js
@@ -1,8 +1,128 @@
 contents.status = {
   index: 9,
-  caption: "Status technologii i wdrożeń VHTR",
-  text: '<p id="status"></p>',
+  caption: "Status doświadczeń z technologią VHTR",
+  text: '<p id="status">Do dnia dzisiejszego (luty 2014) zostało uruchomionych 7 reaktorów wysokotemperaturowych, z czego tylko 2 działają do dzisiaj. W 1942 roku Farrington Daniels, profesor chemii na Uniwersytecie Wisconsin zaproponował projekt reaktorów chłodzonych gazem. Pierwsze prace związane z technologią reaktorów wysokotemperaturowych były wykonywane już pod koniec lat 50-tych, co zaowocowało zbudowaniem i uruchomieniem pierwszego reaktora Dragon w Wielkiej Brytanii. Do chwili obecnej, część koncepcji Danielsa wykorzystuje się w obecnych reaktorach VHTR. Miejsca wszystkich 7 reaktorów w historii rozwoju VHTR zostały przedstawione na rys. 1.</p>'+
+  '<div class="thumbnail">'+
+    '<img src="img/timeline.png" alt="Temperatura reaktora w trakcie awarii" class="img-thumbnail" width="80%"/>'+
+    '<div class="caption">Rys. 1. Wykres słupkowy obrazujący czasy konstrukcji (niebieskie obszary), pracy (czerwone) i wyłączenia (białe) wszystkich do tej pory (luty 2014) zbudowanych reaktorów wysokotemperaturowych&nbsp;<a name="b_jaea"></a>.</div>'+
+  '</div>'+
+  '<h1>Porównanie budowy 7 reaktorów</h1>'+
+  '<p>W tabeli zostało porównane 7 dotychczas zbudowanych reaktorów wraz z ich kluczowymi parametrami.</p>'+
+  '<table class="table table-bordered reactor_comparison">'+
+  '<tr class="gray">'+
+    '<th>Nazwa</th>'+
+    '<th>Państwo</th>'+
+    '<th>Moc termiczna<br/>[MW]</th>'+
+    '<th>Gęstość mocy w rdzeniu<br/><span class="nowrap">[MW/m<sup>3</sup>]</span></th>'+
+    '<th>Chłodziwo drugiego stopnia</th>'+
+    '<th>Temperatura<br/>wlotu reaktora<br/>[&deg;C]</th>'+
+    '<th>Temperatura<br/>wylotu reaktora<br/>[&deg;C]</th>'+
+    '<th>Materiał zbiornika</th>'+
+    '<th>Typ rdzenia</th>'+
+    '<th>Lata pracy</th>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>Dragon</td>'+
+    '<td>Wielka Brytania</td>'+
+    '<td>21,5</td>'+
+    '<td>14</td>'+
+    '<td>para wodna</td>'+
+    '<td>350</td>'+
+    '<td>750</td>'+
+    '<td>stal niestopowa</td>'+
+    '<td>pryzmatyczny</td>'+
+    '<td>1964-1975</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>Peach Bottom</td>'+
+    '<td>USA</td>'+
+    '<td>115</td>'+
+    '<td>8,3</td>'+
+    '<td>para wodna</td>'+
+    '<td>327</td>'+
+    '<td>700-726</td>'+
+    '<td>stal niestopowa</td>'+
+    '<td>pryzmatyczny</td>'+
+    '<td>1966-1974</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>AVR</td>'+
+    '<td>Niemcy</td>'+
+    '<td>46</td>'+
+    '<td>2,6</td>'+
+    '<td>para wodna</td>'+
+    '<td>275</td>'+
+    '<td>950</td>'+
+    '<td>stal i beton</td>'+
+    '<td>kulowy</td>'+
+    '<td>1967-1988</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>Fort <span class="nowrap">St. Vrain</span></td>'+
+    '<td>USA</td>'+
+    '<td>842</td>'+
+    '<td>6,3</td>'+
+    '<td>para wodna</td>'+
+    '<td>404</td>'+
+    '<td>777</td>'+
+    '<td>strunobeton</td>'+
+    '<td>pryzmatyczny</td>'+
+    '<td>1976-1989</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>THTR-300</td>'+
+    '<td>Niemcy</td>'+
+    '<td>750</td>'+
+    '<td>6</td>'+
+    '<td>para wodna</td>'+
+    '<td>404</td>'+
+    '<td>777</td>'+
+    '<td>strunobeton</td>'+
+    '<td>kulowy</td>'+
+    '<td>1985-1991</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>HTTR</td>'+
+    '<td>Japonia</td>'+
+    '<td>30</td>'+
+    '<td>2,5</td>'+
+    '<td>hel / woda pod ciśnieniem</td>'+
+    '<td>395</td>'+
+    '<td>850-950</td>'+
+    '<td>stal <span class="nowrap">2,25% Cr</span>, 1% Mo</td>'+
+    '<td>pryzmatyczny</td>'+
+    '<td>1998-teraz</td>'+
+  '</tr>'+
+  '<tr>'+
+    '<td>HTR-10</td>'+
+    '<td>Chiny</td>'+
+    '<td>10</td>'+
+    '<td>2</td>'+
+    '<td>para wodna</td>'+
+    '<td>250</td>'+
+    '<td>700</td>'+
+    '<td>stal <span class="nowrap">C-Mn-Si</span></td>'+
+    '<td>kulowy</td>'+
+    '<td>2000-teraz</td>'+
+  '</tr>'+
+  '</table>'+
+  '<h2>Dragon</h2>'+
+  '<p>Dragon był pierwszym uruchomionym reaktorem HTGR. Jego zadaniem było testowanie materiałów wytrzymałych na wysokie temperatury. Nie posiadał on urządzeń pozwalających na konwersję energii cieplnej i produkcję energii elektrycznej. Chłodziwem w drugim i trzecim obiegu odprowadzającym ciepło z reaktora była woda oraz para wodna pod ciśnieniem.</p>'+
+  '<h2>Peach Bottom</h2>'+
+  '<p>Peach Bottom był pierwszym reaktorem HTGR uruchomionym w USA i pierwszym na świecie, który produkował energię elektryczną. Pracował bez awarii przez 8 lat jako prototyp demonstrujący produkcję energii elektrycznej z wysoką efektywnoością i cykl paliwowy z wysokowzbogaconym paliwem z uranem i torem. Cykl ten potem został zastosowany w reaktorze Fort St. Vrain. Problemem pojawiającym się w trakcie działania, było uwalnianie produktów rozszczepienia z elementów przebywającycqh najdłużej w rdzeniu. Pod koniec pracy pierwszego rdzenia, w czerwcu 1970 roku, kapsułki zaczynały już puchnąć i pękać. Źródłem tego była konstrukcja kapsułek - rdzeń paliwowy był pokryty tylko jedną warstwą węgla pirolitycznego. W drugim rdzeniu reaktora zastosowano kapsułki z dodatkową warstwą buforową, która absorbowała produkty rozszczepienia. Drugi rdzeń pracował od 1970 do października 1974. Innym problemem, było wdzieranie się oleju do kompresorów helu. Pod koniec pracy rdzenia pierwszego łącznie ok. 100 kg oleju wdarło się do zbiornika reaktora, generuąc trwałe zanieczyszczenia węglem, wodorem i metanem w obiegu pierwotnym. Peach Bottom został zamknięty w październiku 1974 roku, ponieważ korzyści pochodzące z pracy rdzenia drugiego nie przewyższały kosztów utrzymania.</p>'+
+  '<h2>AVR</h2>'+
+  '<p>AVR był pierwszym reaktorem uruchomionym w Niemczech. Pracował on przez 20 lat i osiagał temperatury rzędu 1000 &deg;C - najwyższe jakie osiagały reaktory w tamtym okresie. Największym problemem, jaki pojawił się w trakcie działania reaktora był wyciek z generatora pary wodnej w trakcie jego wyłączenia w 1978 roku. AVR normalnie wykazywał wzrost wilgotnosci helu podczas wyłączenia. Po ponownym uruchomieniu reaktora, poziom wilgotności chłodziwa wzrósł do poziomu wymagającego ponownego zatrzymania pracy reaktora. Śledztwo wykazało nieszczelność o powierzchni 1 - 3 mm<sup>2</sup> w generatorze pary. Ponieważ generatory pary były umieszczone nad rdzenieniem, wyciek spowodował obecność dużej ilości wody w obiegu pierwotnym. AVR nie miał systemu odprowadzania wody, więc usunięcie wody i naprawa generatora pary zajęła 15 miesięcy.</p>'+
+  '<h2>Fort Saint Vrain</h2>'+
+  '<p>Fort St. Vrain (FSV) był jedynym komercyjnym reaktorem w USA. Problemem trapiącym ten reaktor, było wdzieranie się wilgoci do obiegu pierwotnego i ograniczone możliwości jej usuwania. Wilgoć w obiegu pierwotnym stanowiła zagrożenie utleniania elementów grafitowych, korozji elementów stalowych - w szczególności prętów kontrolnych, a także zwiększała reaktywność rdzenia. Głównym źródłem wilgoci w chłodziwie, były komory chłodzące i smarowane wodą łożyska. Dodatkowo niewielki wkład wnosiły generatory i ogrzewacze pary wodnej. Możliwości usuwania wody z obiegu pierwotnego były ograniczone, ponieważ układ oczyszczający hel miał małą przepustowość. Wraz z czasem działania FSV, koszty pracy, napraw i paliwa rosły, aż w roku 1988 podjęto decyzję o jego zamknięciu z powodu nierentowności. </p>'+
+  '<h2>THTR-300</h2>'+
+  '<p>Thorium High Temperature Reactor (THTR) pracował przez 3 lata, po czym został wyłaczony z powodów rosnących kosztów utrzymania. W 1989 firma THTR prawie zbankrutowała po wykryciu usterek elementów kanału gorącego helu - konieczne było wsparcie niemieckiego rządu kwotą 92 milionów marek.</p>'+
+  '<h2>HTTR</h2>'+
+  '<p>Hight Temperature Test Reactor jest jednym z dwóch działających obecnie (stan na luty 2014) reaktorów wysokotemperaturowych. Jest to reaktor wyłacznie badawczy - nie posiada układów pozwalających na konwersję energii. W drugim obiegu chłodniczym znajduje się hel, który ogrzewa wodę pod ciśnieniem, która oddaje ciepło do otoczenia.</p>'+
+  '<h2>HTR-10</h2>'+
+  '<p>HTR-10 jest reaktorem wysokotemperaturowym pracującym w Chinach. Układ elementów reaktora jest identyczny jak w reaktorach które już istniały. Konstrukcja trójzbiornikowa i kanał z gorącym helem łączący reaktor z wymiennikiem ciepła były już stosowane od Peach Bottom. Od 2011 roku, zbiornik konwersji energii zawiera generator pary, ale w planach jest umieszczenie zamiast niego pośredniego wymiennika pary (IHX) w już istniejące miejsce w zbiorniku. Inną możliwością jest zainstalowanie turbiny wykorzystującej cykl Braytona, albo rozbiórka generatora pary i umieszczenie tam turbiny wykorzystujacej bezpośredni cykl Braytona. Pośrednia turbina może używać azotu albo helu jako gazu roboczego. </p>',
   bibliography: {
-    vhtr_rep: '<a href="http://large.stanford.edu/courses/2013/ph241/kallman1/docs/chapin.pdf"><i>A Very High Temperature Reactor: A Technical Summary</i></a>'
+    celin: 'Z. Celiński: <i>Energetyka Jądrowa</i> ',
+    jaea: '<a href="http://www.jaea.go.jp/jaeri/english/ff/ff43/randd01.html"><i>High temperature gas-cooled reactor in Japan reached initial criticality</i> - Japan Atomic Energy Agency (JAEA)</a>',
+    mdow: '<a href="http://www.pnnl.gov/main/publications/external/technical_reports/PNNL-20869.pdf">B.K. Mcdowell et al.: <i>High Temperature Gas Reactors: Assessment of Applicable Codes and Standards</i></a>'
   }
 };
